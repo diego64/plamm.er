d7d1fa31452209c16bae9e2c3e174f5f893e8b15
diego64 <diegoferreira1964@gmail.com>
2024-07-11 15:29:46 -0300

    fix: Configuring eslint in the project

--- api/src/lib/dayjs.ts
+++ api/src/lib/dayjs.ts
@@ -1,8 +1,8 @@
-import dayjs from 'dayjs';
+import dayjs from 'dayjs'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
 import 'dayjs/locale/pt-br'
 
 dayjs.locale('pt-br')
-dayjs.extend(localizedFormat);
+dayjs.extend(localizedFormat)
 
 export { dayjs }
--- api/src/routes/create-invite.ts
+++ api/src/routes/create-invite.ts
@@ -1,59 +1,62 @@
-import type { FastifyInstance } from "fastify";
-import type { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from 'zod';
-import nodemailer from 'nodemailer';
-import { prisma } from "../lib/prisma";
-import { dayjs } from "../lib/dayjs";
-import { getMailClient } from "../lib/mail";
-import { ClientError } from "../errors/client-error";
-import { env } from "../env";
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import nodemailer from 'nodemailer'
+import { prisma } from '../lib/prisma'
+import { dayjs } from '../lib/dayjs'
+import { getMailClient } from '../lib/mail'
+import { ClientError } from '../errors/client-error'
+import { env } from '../env'
 
 export async function createInvite(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().post('/trips/:tripId/invites', {
-    schema: {
-      params: z.object({ 
-        tripId: z.string().uuid(),
-      }),
-      body: z.object({
-        email: z.string().email(),
-      })
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/trips/:tripId/invites',
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+        body: z.object({
+          email: z.string().email(),
+        }),
+      },
     },
-  }, async (request) => {
-    const { tripId } = request.params;
-    const { email } = request.body;
+    async (request) => {
+      const { tripId } = request.params
+      const { email } = request.body
 
-    const trip = await prisma.trip.findUnique({
-      where: { id: tripId },
-      include: {
-        links: true,
-        }
-    })
+      const trip = await prisma.trip.findUnique({
+        where: { id: tripId },
+        include: {
+          links: true,
+        },
+      })
 
-    if(!trip) {
-      throw new ClientError('Trips not found')
-    }
-
-    const participant = await prisma.participant.create({
-      data: {
-        email,
-        trip_id: tripId,
+      if (!trip) {
+        throw new ClientError('Trips not found')
       }
-    })
 
-    const formattedStartDate = dayjs(trip.starts_at).format('LL');
-    const formattedEndDate = dayjs(trip.ends_at).format('LL');
+      const participant = await prisma.participant.create({
+        data: {
+          email,
+          trip_id: tripId,
+        },
+      })
 
-    const mail = await getMailClient();
-        const confirmationLink = `${env.API_BASE_URL}/participants/${participant.id}/confirm`;
+      const formattedStartDate = dayjs(trip.starts_at).format('LL')
+      const formattedEndDate = dayjs(trip.ends_at).format('LL')
 
-        const message = await mail.sendMail({
-          from: {
-            name: 'Equipe plann.er',
-            address: 'oi@plann.er.com'
-          },
-          to: participant.email,
-          subject: `Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
-          html: `
+      const mail = await getMailClient()
+      const confirmationLink = `${env.API_BASE_URL}/participants/${participant.id}/confirm`
+
+      const message = await mail.sendMail({
+        from: {
+          name: 'Equipe plann.er',
+          address: 'oi@plann.er.com',
+        },
+        to: participant.email,
+        subject: `Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
+        html: `
             <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
               <p>Você foi convidado(a) para participar de uma viagem para <strong>${trip.destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
               <p></p>
@@ -65,10 +68,11 @@ export async function createInvite(app: FastifyInstance) {
               <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
             </div>
           `.trim(),
-        });
+      })
 
-        console.log(nodemailer.getTestMessageUrl(message));
+      console.log(nodemailer.getTestMessageUrl(message))
 
       return { participant: participant.id }
-  })
+    },
+  )
 }
